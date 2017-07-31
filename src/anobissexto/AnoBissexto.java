c837dd15d26363f17f92f88e58edcf68c0bd999a
AnaGoncalo <anagoncalo97@gmail.com>
2017-07-31 08:32:55 -0300

    Recomeçando projeto com TDD

--- src/anobissexto/AnoBissexto.java
+++ src/anobissexto/AnoBissexto.java
@@ -14,19 +14,5 @@ public class AnoBissexto {
         
     }
     
-    public static boolean EhBissexto(int ano){
-        try{
-            if(ano < 0)
-                throw new Exception("Ano deve ser positivo");
-        }
-        catch(Exception e){
-            
-        }
-        if(ano % 400 == 0)
-            return true;
-        else if((ano % 4 == 0) && !(ano % 100 == 0))
-            return true;
-        
-        return false;
-    }
+    
 }
--- test/anobissexto/AnoBissextoTest.java
+++ test/anobissexto/AnoBissextoTest.java
@@ -10,21 +10,4 @@ import org.junit.Test;
  */
 public class AnoBissextoTest extends TestCase{
     
-    public void testAnoPositivoDivisivelPor400(){
-        assertEquals(AnoBissexto.EhBissexto(1600), true);  
-    }
-    
-    public void testAnoPositivoDivisivelPor4eNao100(){
-        assertEquals(AnoBissexto.EhBissexto(1732), true);  
-    }
-    
-    @Test(expected = Exception.class)
-    public void testAnoNaoPositivo(){
-        AnoBissexto.EhBissexto(-1);
-    }
-    
-    public void testAnoNaoDivisivelPor4(){
-        assertEquals(AnoBissexto.EhBissexto(2011), false);
-    }
-    
 }
